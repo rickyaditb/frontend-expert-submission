d78ddd2d7f8f83cfe26cb6eda6bb7d38a3fc6112
rickyaditb <rickyaditb@gmail.com>
2024-03-12 22:23:30 +0700

    feat: display data dynamically

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import './components/app-header';
 import './components/home-hero';
 import './components/home-content';
 import './components/app-footer';
+import '../public/data/DATA.json';
 
 const main = async () => {
   const drawerIcon = document.querySelector('.drawer-icon');

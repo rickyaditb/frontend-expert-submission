16dccba43e50984863d5eebdcae967f242e8f842
rickyaditb <rickyaditb@gmail.com>
2024-04-08 23:34:05 +0700

    feat: create card template

---
+++ src/scripts/components/app-card.js
@@ -0,0 +1,30 @@
+import CONFIG from '../globals/config';
+
+const cardTemplate = (restaurant) => `
+  <a href="#/detail/${restaurant.id}" aria-label="Detail Restoran ${restaurant.name}">
+    <article id="${restaurant.id}">
+      <div class="thumbnail-container">
+        <img src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="Gambar Restoran ${restaurant.name}">
+      </div>
+      <div class="card-container">
+        <h3>${restaurant.name}</h3>
+        <p class="truncate">${restaurant.description}</p>
+      </div>
+      <div class="card-stats">
+        <div class="star-container">
+          <div class="star" style="width: ${(restaurant.rating / 5) * 100}%">★★★★★</div>
+        </div>
+        <div class="city">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="city-icon">
+            <path fill-rule="evenodd"
+              d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+              clip-rule="evenodd" />
+          </svg>
+          <span>${restaurant.city}</span>
+        </div>
+      </div>
+    </article>
+  </a>
+`;
+
+export default cardTemplate;
--- src/scripts/components/home-content.js
+++ src/scripts/components/home-content.js
@@ -1,10 +1,11 @@
 import DicodingSource from '../data/dicoding-source';
-import CONFIG from '../globals/config';
+import appCard from './app-card';
 
 class HomeContent extends HTMLElement {
   async connectedCallback() {
     await this._fetchData();
     this.render();
+    this._afterRender();
   }
 
   async _fetchData() {
@@ -17,35 +18,16 @@ class HomeContent extends HTMLElement {
       <h2 class="caption">Katalog Restoran Kami</h2>
       <p class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, earum!</p>
       <section id="catalog">
-        ${this._restaurant.map((item) => `
-        <a href="#/detail/${item.id}" aria-label="Detail Restoran ${item.name}">
-          <article id="${item.id}">
-            <div class="thumbnail-container">
-              <img src="${CONFIG.BASE_IMAGE_URL + item.pictureId}" alt="Gambar Restoran ${item.name}">
-            </div>
-            <div class="card-container">
-              <h3>${item.name}</h3>
-              <p class="truncate">${item.description}</p>
-            </div>
-            <div class="card-stats">
-              <div class="star-container">
-                <div class="star" style="width: ${(item.rating / 5) * 100}%">★★★★★</div>
-              </div>
-              <div class="city">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="city-icon">
-                  <path fill-rule="evenodd"
-                    d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-                    clip-rule="evenodd" />
-                </svg>
-                <span>${item.city}</span>
-              </div>
-            </div>
-          </article>
-        </a>
-        `).join('')}
       </section>
     </section>
     `;
+  }
+
+  _afterRender() {
+    const catalog = document.getElementById('catalog');
+    this._restaurant.forEach((restaurant) => {
+      catalog.innerHTML += appCard(restaurant);
+    });
   }
 }
 

a240c72f4173eabd93e528855ba975743083c677
rickyaditb <rickyaditb@gmail.com>
2024-04-12 01:54:14 +0700

    feat: add e2e testing

--- e2e/Liking_Restaurants.test.js
+++ e2e/Liking_Restaurants.test.js
@@ -1,15 +1,51 @@
+const assert = require('assert');
+
 /* eslint-disable no-undef */
 Feature('Liking Restaurants');
 
-Before(({ I }) => {
-  I.amOnPage('/#/favorite');
-});
-
 Scenario('Show empty liked restaurants', ({ I }) => {
-  I.see('Belum ada restoran favorit yang ditambahkan :(', 'p');
+  I.amOnPage('/#/favorite');
+  I.see('Ooppsss, belum ada restoran yang ditambahkan ke favorit.', '.empty-message > p:nth-child(2)');
 });
 
-Scenario('liking one movie', ({ I }) => {
-  I.amOnPage('/');
-  // ... kita akan mengisi uji coba berikutnya ...
+Scenario('liking one restaurants', async ({ I }) => {
+  I.amOnPage('/#/');
+  I.waitForElement('article', 30);
+  I.seeElement('article');
+  const firstRestaurant = locate('article h3').first();
+  const firstRestaurantText = await I.grabTextFrom(firstRestaurant);
+  I.click(locate('article').first());
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.waitForElement('article', 30);
+  I.seeElement('article');
+  const likedRestaurantText = await I.grabTextFrom('article h3');
+  assert.strictEqual(firstRestaurantText, likedRestaurantText);
+});
+
+Scenario('unliking one restaurants', async ({ I }) => {
+  I.amOnPage('/#/');
+  I.waitForElement('article', 30);
+  I.seeElement('article');
+  const firstRestaurant = locate('article').first();
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.waitForElement('article', 30);
+  I.seeElement('article'); // check if restaurant is already added to favorite
+
+  const likedRestaurant = locate('article');
+  I.click(likedRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.see('Ooppsss, belum ada restoran yang ditambahkan ke favorit.', '.empty-message > p:nth-child(2)');
 });

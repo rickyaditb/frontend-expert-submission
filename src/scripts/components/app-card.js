8f361ccbf4ba7441f3c1f0b285dfe7d7d682529d
rickyaditb <rickyaditb@gmail.com>
2024-04-16 02:40:56 +0700

    feat: implement responsive image

--- src/scripts/components/app-card.js
+++ src/scripts/components/app-card.js
@@ -4,7 +4,11 @@ const cardTemplate = (restaurant) => `
   <a href="#/detail/${restaurant.id}" aria-label="Detail Restoran ${restaurant.name}">
     <article id="${restaurant.id}">
       <div class="thumbnail-container">
-        <img class="lazyload" src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="Gambar Restoran ${restaurant.name}">
+        <picture>
+          <source media="(max-width: 600px)" srcset="${`${CONFIG.BASE_IMAGE_URL}small/${restaurant.pictureId}`}">
+          <source media="(max-width: 1280px)" srcset="${`${CONFIG.BASE_IMAGE_URL}medium/${restaurant.pictureId}`}">
+          <img class="lazyload" src="${`${CONFIG.BASE_IMAGE_URL}large/${restaurant.pictureId}`}" alt="Gambar Restoran ${restaurant.name}">
+        </picture>
       </div>
       <div class="card-container">
         <h3>${restaurant.name}</h3>
